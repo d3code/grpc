76f6290a8efcedf77f0248f075aa43a507c6cbc8
Luke Sands <luke.sands@d3code.com.au>
2023-10-03 12:20:06 +1100

    Add request ID to middleware log
    This commit adds a request ID to the middleware log for each incoming request. The request ID is generated using the Google UUID package and is set as a header in the request. The log message now includes the request ID along with other information such as the method, URI, protocol, host, and headers. This will make it easier to track and debug requests in the logs. Additionally, the log message for the elapsed time now includes the request ID.

--- server/http_middleware.go
+++ server/http_middleware.go
@@ -2,6 +2,7 @@ package server
 
 import (
     "github.com/d3code/zlog"
+    "github.com/google/uuid"
     "net/http"
     "strings"
     "time"
@@ -11,10 +12,30 @@ import (
 func middlewareLog(next http.Handler) http.Handler {
     handler := func(w http.ResponseWriter, r *http.Request) {
         start := time.Now()
+
+        requestId := uuid.New().String()
+        r.Header.Set("X-Request-Id", requestId)
+
+        zlog.Log.Infow("Request received [ "+r.RequestURI+" ]",
+            "Request ID", requestId,
+            "Method", r.Method,
+            "URI", r.RequestURI,
+            "Protocol", r.Proto,
+            "Host", r.Host,
+            "Accept", r.Header.Get("Accept"),
+            "Accept encoding", r.Header.Get("Accept-Encoding"),
+            "Accept language", r.Header.Get("Accept-Language"),
+            "Content type", r.Header.Get("Content-Type"),
+            "Authorization", r.Header.Get("Authorization"),
+            "Content length", r.ContentLength,
+            "Remote address", r.RemoteAddr,
+            "User agent", r.UserAgent(),
+            "Referer", r.Referer())
+
         next.ServeHTTP(w, r)
 
         elapsed := time.Since(start)
-        zlog.Log.Infof("Request to [ %s ] processed in %s", r.RequestURI, elapsed)
+        zlog.Log.Infof("Request ID [ %s ] processed in %s", requestId, elapsed)
     }
     return http.HandlerFunc(handler)
 }
@@ -42,7 +63,7 @@ func middlewareCORS(h http.Handler) http.Handler {
                 w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
                 w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
 
-                zlog.Log.Infof("Preflight request for %s", r.URL.Path)
+                zlog.Log.Infof("Preflight request for [ %s ]", r.URL.Path)
                 return
             }
         }
